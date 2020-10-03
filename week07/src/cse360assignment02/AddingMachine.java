53451bd683a0acba96cd0849d910b46a9bbc7d13
chopi <chopi@DESKTOP-S3T6PV3>
2020-10-03 14:28:51 -0700

    Changes added:
    - global variable toString added to store recorded history
    - getTotal method implemented
    - add method implemented
    - subtract method implemented
    - toString method implemented
    - clear method implemented

--- week07/src/cse360assignment02/AddingMachine.java
+++ week07/src/cse360assignment02/AddingMachine.java
@@ -1,26 +1,34 @@
 package cse360assignment02;
 
 public class AddingMachine {
-  private int total;
-  
-  public AddingMachine () {
-    total = 0;  // not needed - included for clarity
-  }
-  
-  public int getTotal () {
-    return 0;
-  }
-  
-  public void add (int value) {
-  }
+	private int total;
+	private String toString;
 
-  public void subtract (int value) {
-  }
+	public AddingMachine() {
+		total = 0; //not needed - included for clarity
+		toString = "0";
+	}
 
-  public String toString () {
-    return "";
-  }
+	public int getTotal() {
+		return total;
+	}
 
-  public void clear() {
-  }
+	public void add(int value) {
+		total = value + total;
+		toString = toString + " + " + Integer.toString(value);
+	}
+
+	public void subtract(int value) {
+		total = total - value;
+		toString = toString + " - " + Integer.toString(value);
+	}
+
+	public String toString() {
+		return toString;
+	}
+
+	public void clear() {
+		total = 0;
+		toString = "0";
+	}
 }
